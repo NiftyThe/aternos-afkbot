68d6043fb733187c760edfd03b3fa4151e26df16
NiftyThe <sajaffri09@gmail.com>
2024-12-20 16:05:52 +0500

    Update index.js

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ var lastaction;
 var pi = 3.14159;
 var moveinterval = 2; // 2 second movement interval
 var maxrandom = 5; // 0-5 seconds added to movement interval (randomly)
-var host = data["ip"];
-var port = data["port"];
-var username = data["name"]
+var host = data["AimsSMPx.aternos.me"];
+var port = data["12270"];
+var username = data["JaffriKaBall1"]
 var bot = mineflayer.createBot({
   host: host,
   port: port,
